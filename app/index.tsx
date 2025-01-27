7feb5e34a4a80262bf8b599b01d48b0b28aab9b2
weswwn <wesley.qb.wen@gmail.com>
2025-01-26 17:02:20 -0800

    add base magic link OTP

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,24 @@
 import { Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import { supabase } from "../client/supabase";
+import { Login } from "../components/login";
+import { Session } from "@supabase/supabase-js";
 import "../global.css";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  const isLoggedIn = session && session.user;
+
+  return <View>{!isLoggedIn && <Login />}</View>;
 }
