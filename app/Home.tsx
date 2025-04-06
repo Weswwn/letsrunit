2a746b7d8f7fd835621a5d97d839053abff84f5a
weswwn <wesley.qb.wen@gmail.com>
2025-04-05 17:41:31 -0700

    wip notification commit

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,14 +1,21 @@
 import { useContext } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { ProfileContext } from "./index";
+import { sendPushNotification } from "@/client/services/pushNotifications";
 
-const Home = () => {
+const Home = ({ pushToken }: { pushToken: string }) => {
   console.log("profile context", ProfileContext);
   const profile = useContext(ProfileContext);
   console.log("profile:", profile);
   return (
     <View>
       <Text>Hello, you're logged in</Text>
+      <Button
+        title="Press to Send Notification"
+        onPress={async () => {
+          await sendPushNotification(pushToken);
+        }}
+      />
     </View>
   );
 };
